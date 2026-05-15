4565afd84f3e8748e8f4bf8129dc0a40d08dae76
Reya-Doshi <reyadoshi23@gmail.com>
2026-05-15 19:45:47 +0530

    Backend Connection

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,79 @@
+import bcrypt from "bcrypt";
+import prisma from "../config/db.js";
+import generateToken from "../utils/generateToken.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    // Generate token
+    const token = generateToken(user.id);
+
+    res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    // Check user and password
+    if (
+      user &&
+      (await bcrypt.compare(password, user.password))
+    ) {
+      res.status(200).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
