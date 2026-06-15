94fd01406039d53916c42e2e22b9429c0f6472d4
VaishnaviPatil-gif <vaishnavipatil8096@gmail.com>
2026-06-15 16:52:20 +0530

    chore: register goals recommendations and simulation routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,9 @@ import analysisRoutes from "./routes/analysisRoutes.js";
 import progressRoutes from "./routes/progressRoutes.js";
 import interviewRoutes from "./routes/interviewRoutes.js";
 import projectsRoutes from "./routes/projectsRoutes.js";
+import whatifRoutes from "./routes/whatifRoutes.js";
+import goalsRoutes from "./routes/goalsRoutes.js";
+import recommendationsRoutes from "./routes/recommendationsRoutes.js";
 import passport from "./config/passport.js";
 
 const app = express();
@@ -40,6 +43,12 @@ app.use("/api/progress", progressRoutes);
 app.use("/api/interviews", interviewRoutes);
 
 app.use("/api/projects", projectsRoutes);
+
+app.use("/api/whatif", whatifRoutes);
+
+app.use("/api/goals", goalsRoutes);
+
+app.use("/api/recommendations", recommendationsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
